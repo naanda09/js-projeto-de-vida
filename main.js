d989ae2f8b7103c54598d6113c50275a523d8ce9
naanda09 <159458104+naanda09@users.noreply.github.com>
2026-04-07 14:15:08 +0000

    regressiva

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+
+contadores[0].textContent = "Contagem Regressiva"
 
 for(let i = 0; i < botoes.length; i = i + 1){
     botoes[i].onclick = function(){
         for(let j = 0; j < botoes.length; j = j + 1){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
+        botoes[i].classList.add("ativo");
         botoes[i].classList.add("ativo");
     }
 
